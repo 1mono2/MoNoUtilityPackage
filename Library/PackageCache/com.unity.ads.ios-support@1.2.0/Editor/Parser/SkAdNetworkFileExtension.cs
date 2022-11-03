7425c608f74117d9b911f85d650e1acf0c0948c7
1mono2onMac <taka19980618@gmail.com>
2022-11-04 00:00:02 +0900

    faat: exclude others packages

--- Library/PackageCache/com.unity.ads.ios-support@1.2.0/Editor/Parser/SkAdNetworkFileExtension.cs
+++
@@ -1,9 +0,0 @@
-namespace Unity.Advertisement.IosSupport.Editor
-{
-    public static class SkAdNetworkFileExtension
-    {
-        public const string XML = "xml";
-        public const string JSON = "json";
-        public const string NONE = "";
-    }
-}
